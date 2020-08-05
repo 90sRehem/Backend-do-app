3faadc6314dc5d6cdb4ef230d0b6bb4cf54a03fd
Jonathan Rehem <jonathanrehem@moveapps.com.br>
2020-08-05 09:09:15 -0300

    implementando DDD

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,9 +5,9 @@ import cors from 'cors'
 import 'express-async-errors'
 
 import routes from './routes'
-import AppError from './errors/AppError'
-import uploadConfig from './config/upload'
-import './database'
+import AppError from '../../errors/AppError'
+import uploadConfig from '../../../config/upload'
+import '../typeorm'
 
 const app = express()
 
