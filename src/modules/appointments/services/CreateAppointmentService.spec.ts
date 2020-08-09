89579e13aeef074968496e1d133614c999d22d77
Jonathan Rehem <jonathan.de.oliveira@live.com>
2020-08-09 20:12:36 -0300

    Implentação de testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,33 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+import AppError from '@shared/errors/AppError';
+
+describe('CreateAppointment', async () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '123'
+        })
+        expect(appointment).toHaveProperty('id')
+        expect(appointment.provider_id).toBe('123')
+    });
+
+    it('should not be able to create two apppointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+
+        const appointmentDate = new Date(2020, 8, 9, 18)
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123'
+        })
+
+        expect(createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123'
+        })).rejects.toBeInstanceOf(AppError)
+    })
+})
