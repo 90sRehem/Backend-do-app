07ec1d4afc3b52f5648478e3196ff26f3b270396
Jonathan Rehem <jonathanrehem@moveapps.com.br>
2020-08-19 17:19:48 -0300

    forgot password test

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import { injectable, inject } from 'tsyringe'
+
+import User from '../infra/typeorm/entities/User'
+import AppError from '@shared/errors/AppError'
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository'
+
+interface IRequest {
+    email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) { }
+
+    public async execute(data : IRequest): Promise<void> {
+
+       
+    }
+}
+
+export default SendForgotPasswordEmailService
