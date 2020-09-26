f68a924c092de9e9204912b4c30f4b07818a15c9
Jonathan Rehem <jonathan.de.oliveira@live.com>
2020-09-26 13:02:25 -0300

    finished

--- src/modules/appointments/services/ListProviderDayAvailability.ts
+++ src/modules/appointments/services/ListProviderDayAvailability.ts
@@ -34,10 +34,11 @@ class LisProviderDayAvailability {
 
         const eachHourArray = Array.from({ length: 10 }, (_, index) => index + hourStart)
 
+        const currentDate = new Date(Date.now())
+
         const availability = eachHourArray.map(hour => {
             const hasAppointmentInHour = appointments.find(appointment => getHours(appointment.date) === hour)
 
-            const currentDate = new Date(Date.now())
             const compareDate = new Date(year, month - 1, day, hour)
 
             return {
